d7ac0a71d6d5e8fa449ea1aa710ab21549bd9f49
Stacy Burris <stacy1burris@gmail.com>
2020-10-02 16:35:52 -0700

    finished question two

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ function sum(a, b) { //eslint-disable-line
 testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
-
 /////////////////////////////////////
 /* Problem 2
 Write a function called multiply() that takes in two numbers as arguments and returns an array where the first element is the product of those numbers, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
@@ -29,11 +28,13 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var totalMultiply = a * b;
+  var string = 'The product of ' + a + ' and ' + b + ' is ' + totalMultiply + '.';
+  return [totalMultiply, string];
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
