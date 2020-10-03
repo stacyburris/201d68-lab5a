a98347bb4f3aea02243059af6d7d7e1e5a84cc62
Stacy Burris <stacy1burris@gmail.com>
2020-10-02 20:20:14 -0700

    finished challange questionfive

--- app.js
+++ app.js
@@ -113,11 +113,16 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+  var multArrOne = multiply(testArray[0], testArray[1]); // 2 * 3 = 6
+  
+  var multArrTwo = multiply(multArrOne[0], testArray[2]);
 
+  var string = 'The numbers ' + testArray + ' have a product of ' + multArrTwo[0] + '.';
+  return [multArrTwo[0], string];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
